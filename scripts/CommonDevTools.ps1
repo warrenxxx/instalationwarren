b02ba726bcc1b5a76489c542f69e7a8c01e0be5e
warren aroni <warren.aroni@provistacorp.com>
2019-03-29 14:15:58 -0500

    first commit

---
+++ scripts/CommonDevTools.ps1
@@ -0,0 +1,12 @@
+
+# tools we expect devs across many scenarios will want
+choco install -y vscode
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y python
+choco install -y sysinternals
+choco install -y jre8
+choco install -y nodejs.install
+choco install -y python2
+choco install -y netfx-4.7.1-devpack
+choco install -y notepadplusplus.install
+choco install -y cmder
---
+++ scripts/JetBrains.ps1
@@ -0,0 +1,4 @@
+choco install -y webstorm
+choco install -y intellijidea-ultimate
+choco install -y jetbrains-rider
+choco install -y datagrip
